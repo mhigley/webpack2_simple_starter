c79b20767791e006726ec6001c1b33676a720ece
Matthew Higley <matthewhigley@MattyBook-Pro.local>
2017-03-30 13:15:46 -0700

    pushing webpack2

---
+++ js/index.js
@@ -0,0 +1,5 @@
+import sum from './sum';
+import '../styles/style.scss';
+
+const total = sum(15, 5);
+console.log(total);
---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+    entry: './js/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js',
+        publicPath: 'build/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: 'babel-loader'
+            },
+            {
+                test: /\.(css|scss)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: ['css-loader', 'sass-loader']
+                })
+            }
+        ]
+    },
+    plugins: [
+        new ExtractTextPlugin('style.css')
+    ]
+};
+
+module.exports = config;
